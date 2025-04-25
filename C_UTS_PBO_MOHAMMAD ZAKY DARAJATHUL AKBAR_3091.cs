bd6cd5077602dde45e399be6196083c52f70a07c
KamishBlade <akbarzaky2005@gmail.com>
2025-04-25 14:37:43 +0700

    Add files via upload

---
+++ C_UTS_PBO_MOHAMMAD ZAKY DARAJATHUL AKBAR_3091.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+    public interface id;
+    public string Judul Buku;
+    public string Nama Penulis;
+    public string Tahun Terbit;
+
+class perpustakaan
+(
+    public void perpustakaan ()
+
+        Console.WriteLine("Mengelola Koleksi Buku");
+        Console.WriteLine("Informasi Buku");
+)
+class book
+{
+    static void book.Main()
+    {
+        Console.WriteLine("1 ID");
+        Console.WriteLine("2. Judul Buku :v");
+        Console.WriteLine("3.Nama Penulis ");
+        Console.WriteLine("4. Tahun Terbit :)\n");
+    }
+}
+
+static class book
+{
+    public static void ()
+    {
+        string Dir = @"..\..\..\id";
+        string[] files = Directory.GetFiles(Dir, "*.txt");
+        Console.WriteLine("┌─B");
+        Console.WriteLine("│");
+        Console.WriteLine("│");
+
+        {
+            Console.WriteLine($"├{Path.GetFileName(file)}");
+            Console.Write($"├─{File.ReadAllText(file)}\n");
+        }
+        Console.WriteLine("│");
+        Console.WriteLine("└─O");
+
+
+    }
+}
+
